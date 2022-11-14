792faf0e1277780954c9c3bd858ee5d3261d6c1b
shahram1371 <shahram.faraji71@gmail.com>
2022-11-14 23:19:58 +0330

    fix interace

--- lib/main.dart
+++ lib/main.dart
@@ -77,13 +77,19 @@ class MyHomePageState extends State<MyHomePage> {
         }),
       );
     } else {
-      setState(() => _isListening = false);
+      setState(() => _isListening = true);
       _speechToText.stop();
       _recorder();
     }
+    setState(() {
+      _isListening = false;
+    });
     await Future.delayed(const Duration(seconds: 3)).then((value) {
       _speechToText.stop();
       _recorder();
+      setState(() {
+        _isListening = true;
+      });
     });
   }
 
@@ -139,13 +145,21 @@ class MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ),
-              MaterialButton(
-                onPressed: _recorder,
-                color: _isListening ? Colors.grey : Colors.pink,
-                textColor: Colors.white,
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(25),
-                child: const Icon(Icons.mic, size: 60),
+              Column(
+                children: [
+                  MaterialButton(
+                    onPressed: _recorder,
+                    color: !_isListening ? Colors.grey : Colors.pink,
+                    textColor: Colors.white,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(25),
+                    child: const Icon(Icons.mic, size: 60),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  !_isListening ? const Text('please speak') : Container()
+                ],
               ),
               Text(
                 text,
@@ -197,3 +211,123 @@ class MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
+///due to train
+// import 'package:flutter/material.dart';
+// import 'package:tflite_audio/tflite_audio.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData.dark(),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   MyHomePageState createState() => MyHomePageState();
+// }
+
+// class MyHomePageState extends State<MyHomePage> {
+//   String _sound = "Press the button to start";
+//   bool _recording = false;
+//   late Stream<Map<dynamic, dynamic>> result;
+//   int inferenceTime = 0;
+//   @override
+//   void initState() {
+//     super.initState();
+//     TfliteAudio.loadModel(
+//       inputType: 'rawAudio',
+//       model: 'assets/soundclassifier_with_metadata.tflite',
+//       label: 'assets/labels.txt',
+//       outputRawScores: false,
+//       numThreads: 1,
+//       isAsset: true,
+//     );
+//     _recorder();
+//   }
+
+//   void _recorder() {
+//     String recognition = "";
+//     // if (!_recording) {
+//     setState(() => _recording = true);
+//     result = TfliteAudio.startAudioRecognition(
+//         detectionThreshold: 0.5,
+//         sampleRate: 44100,
+//         bufferSize: 22016,
+//         numOfInferences: 2);
+
+//     result.listen((event) {
+//       recognition = event["recognitionResult"];
+//     }).onDone(() {
+//       setState(() {
+//         _recording = false;
+//         _sound = recognition.split(" ")[1];
+//         print(_sound);
+//         // if (_sound == 'Hello') {
+
+//         // }
+//       });
+//       _recorder();
+//     });
+//     // }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage("assets/background.jpg"),
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: <Widget>[
+//               Container(
+//                 padding: const EdgeInsets.all(20),
+//                 child: const Text(
+//                   "What's this sound?",
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 60,
+//                     fontWeight: FontWeight.w200,
+//                   ),
+//                 ),
+//               ),
+//               MaterialButton(
+//                 onPressed: _recorder,
+//                 color: _recording ? Colors.grey : Colors.pink,
+//                 textColor: Colors.white,
+//                 shape: const CircleBorder(),
+//                 padding: const EdgeInsets.all(25),
+//                 child: const Icon(Icons.mic, size: 60),
+//               ),
+//               Text(
+//                 _sound,
+//                 style: Theme.of(context).textTheme.headline5,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
